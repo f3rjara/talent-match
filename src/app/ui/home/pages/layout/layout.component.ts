f1075dc36ea1d14bf434ef4adbd9679863e29919
HolguerAndrade <holguer.andrade@finaktiva.com>
2024-12-11 11:40:12 -0500

    feat: add page vacancies

--- src/app/ui/home/pages/layout/layout.component.ts
+++ src/app/ui/home/pages/layout/layout.component.ts
@@ -1,14 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NgClass, ButtonModule],
+  imports: [
+    ButtonModule,
+    RouterOutlet
+  ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
-  isCheck: boolean = false;
+
 }
