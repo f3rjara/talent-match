2692392a017838f39f3baf89d8f80255d237bdf2
f3rjara <f3rjara@gmail.com>
2024-12-12 13:34:48 -0500

    ajuste en algo

--- src/app/ui/candidate/pages/init-aplication/init-aplication.component.ts
+++ src/app/ui/candidate/pages/init-aplication/init-aplication.component.ts
@@ -32,6 +32,6 @@ export class InitAplicationComponent implements OnInit {
   buildUrlChatboot() {
     const vacancieTitleEncoded = encodeURIComponent(this.vacancieTitle);
     const vancancieIdEncoded = encodeURIComponent(this.vancancieId);
-    return `https://api.whatsapp.com/send?phone=573172396943&text=Hola%20%E2%9C%8C%EF%B8%8F%F0%9F%98%8A%2C%0A%0AMe%20gustar%C3%ADa%20crear%20mi%20curr%C3%ADculum%20con%20su%20ayuda%20y%20postularme%20para%20la%20oferta%20laboral%20disponible%20para%3A%20*${vacancieTitleEncoded}*%20%F0%9F%92%BB%E2%9C%A8.%0A%0A************************%0A%23vacante%3A*${vancancieIdEncoded}%23`;
+    return `https://api.whatsapp.com/send?phone=573172396943&text=Hola%20%E2%9C%8C%EF%B8%8F%F0%9F%98%8A%2C%0A%0AMe%20gustar%C3%ADa%20crear%20mi%20curr%C3%ADculum%20con%20su%20ayuda%20y%20postularme%20para%20la%20oferta%20laboral%20disponible%20para%3A%20*${vacancieTitleEncoded}*%20%F0%9F%92%BB%E2%9C%A8.%0A%0A************************%0A%23vacante%3A${vancancieIdEncoded}%23`;
   }
 }
