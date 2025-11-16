efa11c40a1e696a18fa86fc5e766edce5d2ca34e
Fernando Jaramillo <f3rjara@gmail.com>
2025-11-15 19:29:22 -0500

    chore: update dependencies and refactor app configuration
    - Updated PrimeNG and related themes to version 19.0.4 and added new theme dependencies.
    - Refactored AppComponent to remove unused imports and simplify the component.
    - Modified app.config.ts to include PrimeNG configuration with ripple effect and theme settings.
    - Updated setup-jest.ts to use the new setup function for zone testing.
    - Cleaned up styles.scss by removing unused PrimeNG CSS imports and retaining only necessary styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   template: '<router-outlet />',
   imports: [RouterModule],
 })
-export class AppComponent implements OnInit {
-  private readonly _primengConfig: PrimeNGConfig = inject(PrimeNGConfig);
-  ngOnInit(): void {
-    this._primengConfig.ripple = true;
-  }
-}
+export class AppComponent {}
